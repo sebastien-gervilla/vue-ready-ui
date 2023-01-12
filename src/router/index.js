f847266f04f49a52f38cce81bc4ecea1837b877b
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-01-12 16:39:35 +0100

    Feat: Added pages to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import StoreView from '../views/StoreView.vue';
+import ContactView from '../views/ContactView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +12,15 @@ const router = createRouter({
             component: HomeView,
         },
         {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import('../views/AboutView.vue'),
+            path: '/store',
+            name: 'store',
+            component: StoreView,
         },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: ContactView,
+        }
     ],
 });
 

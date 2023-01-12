3ca101e5f067e5f4902827b3b4b2dc4b93fbac81
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-01-12 16:47:38 +0100

    Feat: Navigation Component

---
+++ src/components/global/Navigation.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navigation">
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/store">Store</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+import Link from './Link.vue';
+export default {
+    components: {
+        Link
+    }
+}
+</script>
+
+<style lang="scss">
+nav.navigation {
+    ul {
+        display: flex;
+
+        li {
+            a {
+                margin: 0 20px;
+
+                font-size: var(--fontsize-normal);
+            }
+        }
+    }
+}
+</style>

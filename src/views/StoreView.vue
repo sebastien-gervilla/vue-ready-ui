5e499760698831a294fd98ca64e17f9185b8afe4
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-01-12 16:39:26 +0100

    Feat: Store Page

---
+++ src/views/StoreView.vue
@@ -0,0 +1,16 @@
+<template>
+    <PageLayout id="store-page">
+        
+    </PageLayout>
+</template>
+
+<script>
+import Button from '../components/global/Button.vue';
+import PageLayout from '../components/global/PageLayout.vue';
+export default {
+    components: {
+        PageLayout,
+        Button
+    }
+}
+</script>
